28e313dcd7b300933ca13f66050696223946043d
Vasiliy Dontsul <dontsul.v@gmail.com>
2023-02-16 23:45:57 +0200

    add gh pages

---
+++ src/components/app/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AppHeader from '../appHeader/AppHeader';
+import MainPage from '../pages/MainPage';
+import ComicsPage from '../pages/ComicsPage';
+import Page404 from '../pages/404';
+import SingleComicPage from '../pages/SingleComicPage';
+
+const App = () => {
+    return (
+        <Router>
+            <div className="app">
+                <AppHeader />
+                <main>
+                    <Routes>
+                        <Route path="/marvel" element={<MainPage />} />
+                        <Route path="/comics" element={<ComicsPage />} />
+                        <Route path="/comics/:comicId" element={<SingleComicPage />} />
+                        <Route path="*" element={<Page404 />} />
+                    </Routes>
+                </main>
+            </div>
+        </Router>
+    );
+};
+
+export default App;
